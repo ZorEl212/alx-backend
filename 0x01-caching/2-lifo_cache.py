656f80ffd4a6a43f3180dc65c42ebdf3944d2c6e
ZorEl212 <yabsirad212@gmail.com>
2024-05-07 12:56:11 -0700

    Implement LIFO Caching system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""""LIFO Cacheing System"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """"Class implementing LIFO Cache"""
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        if item is None or key is None:
+            return None
+        if len(self.cache_data) >= super().MAX_ITEMS:
+            if key not in self.cache_data.keys():
+                k = list(self.cache_data.keys())[-1]
+                self.cache_data.popitem()
+                print(f'DISCARD: {k}')
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
