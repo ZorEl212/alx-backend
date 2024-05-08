8eb6dcaf5e7ee570df2162f18bdd4617c5198e67
Yeabsira Desalegn <93014559+ZorEl212@users.noreply.github.com>
2024-05-08 23:12:12 +0300

    fix method documentations

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -8,21 +8,25 @@ class LIFOCache(BaseCaching):
     """"Class implementing LIFO Cache"""
 
     def __init__(self):
-        super().__init__()
-        self.cache_data = OrderedDict()
+      """init method overriding super's"""
+      super().__init__()
+      self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        if item is None or key is None:
-            return None
-        if len(self.cache_data) >= super().MAX_ITEMS:
-            if key not in self.cache_data.keys():
+      """Put a cache in lifo system"""
+      if item is None or key is None:
+          return None
+      if len(self.cache_data) >= super().MAX_ITEMS:
+          if key not in self.cache_data.keys():
                 k = list(self.cache_data.keys())[-1]
                 self.cache_data.popitem()
                 print(f'DISCARD: {k}')
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key)
+      self.cache_data[key] = item
+      
+      self.cache_data.move_to_end(key)
 
     def get(self, key):
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data[key]
+      """Get a item by key if exists"""
+      if key is None or key not in self.cache_data.keys():
+          return None
+      return self.cache_data[key]
