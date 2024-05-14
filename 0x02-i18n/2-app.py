f0c49f66f0ef3a1a1420857ac137c45628bdf24e
ZorEl212 <yabsirad212@proton.me>
2024-05-15 01:05:57 +0300

    Get user locale
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Simple flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for the app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+def get_locale():
+    """evaluate locale based on user requests"""
+    request.accept_languages.best_match(app.config['LANGUAGES'])
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+@app.route('/')
+
+def index():
+    """Index page route"""
+    return render_template('2-index.html')
+
+ 
+if __name__ == '__main__':
+    app.run()
